7f0ae2094e29698cfa09c9db7e51f14bc4c49b7b
Aidas Eglinskas <aidas.eglinskas@gmail.com>
2018-11-04 12:17:36 +0200

    Installed ADMIN-LTE3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
     // directory where compiled assets will be stored
@@ -18,32 +19,11 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('js/app', './assets/js/app.js')
-    .addEntry('js/adminlte/admin-lte', [
-        './node_modules/admin-lte/plugins/jquery/jquery.min.js',
-        './node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js',
-        './node_modules/admin-lte/plugins/morris/morris.min.js',
-        './node_modules/admin-lte/plugins/sparkline/jquery.sparkline.min.js',
-        './node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
-        './node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
-        './node_modules/admin-lte/plugins/knob/jquery.knob.js',
-        './node_modules/admin-lte/plugins/daterangepicker/daterangepicker.js',
-        './node_modules/admin-lte/plugins/datepicker/bootstrap-datepicker.js',
-        './node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
-        './node_modules/admin-lte/plugins/fastclick/fastclick.js',
-        './node_modules/admin-lte/dist/js/adminlte.js',
-        './node_modules/admin-lte/dist/js/pages/dashboard.js',
-        './node_modules/admin-lte/dist/js/demo.js'
-    ])
 
     .addStyleEntry('css/app', './assets/css/app.scss')
     .addStyleEntry('css/adminlte/admin-lte', [
         './node_modules/font-awesome/css/font-awesome.min.css',
-        './node_modules/admin-lte/dist/css/AdminLTE.min.css',
-        './node_modules/admin-lte/plugins/iCheck/flat/blue.css',
-        './node_modules/admin-lte/plugins/morris/morris.css',
-        './node_modules/admin-lte/plugins/datepicker/datepicker3.css',
-        './node_modules/admin-lte/plugins/daterangepicker/daterangepicker-bs3.css',
-        './node_modules/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'
+        './node_modules/admin-lte/dist/css/adminlte.min.css',
     ])
 
     /*
@@ -67,6 +47,13 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './assets/images', to: 'images'},
+        { from: './node_modules/admin-lte/plugins/jquery/jquery.min.js', to: 'js/adminlte'},
+        { from: './node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js', to: 'js/adminlte'},
+        { from: './node_modules/admin-lte/dist/js/adminlte.min.js', to: 'js/adminlte'}
+    ]))
 ;
 
 module.exports = Encore.getWebpackConfig();
